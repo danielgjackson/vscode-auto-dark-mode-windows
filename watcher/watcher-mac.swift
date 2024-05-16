e63bfc34663947c68ae1df42bfaa8fc8b1d15e71
Daniel Jackson <email@danieljackson.co.uk>
2024-05-16 22:03:29 +0100

    -

---
+++ watcher/watcher-mac.swift
@@ -0,0 +1,31 @@
+// Note: This implementation is not currently used -- the Objective C version (watcher-mac.m) is used instead.
+
+// Output 1 for light theme, 0 for dark theme -- initial value, then any changes.
+
+// swiftc watcher-mac.swift -o watcher-mac
+// swiftc watcher-mac.swift && ./watcher-mac
+
+import Foundation
+
+class Watcher: NSObject {
+    //private static var observerContext = 0
+
+    override init() {
+        super.init()
+        UserDefaults.standard.addObserver(self, forKeyPath: "AppleInterfaceStyle", options: [.initial, .new /*, .old,  .prior*/], context: nil /*&Self.observerContext*/)
+    }
+
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        let interfaceStyle = (change![.newKey] as? String ?? "")
+        if interfaceStyle == "Dark" {
+            print("0")  // Dark
+            fflush(stdout)
+        } else {
+            print("1")  // Light
+            fflush(stdout)
+        }
+    }
+}
+
+let watcher = Watcher()
+RunLoop.main.run()
