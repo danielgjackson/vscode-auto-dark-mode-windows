d832517a0b47c287f89696349b8db41ba4974963
Daniel Jackson <email@danieljackson.co.uk>
2024-05-17 15:20:13 +0100

    -

--- watcher/build-all.cmd
+++ watcher/build-all.cmd
@@ -3,8 +3,6 @@ del watcher-lin-x64 watcher-lin-arm watcher-win.exe watcher-mac
 
 docker buildx build --output type=local,dest=./ .
 
-docker run -v .:/workspace --rm -it ghcr.io/shepherdjerred/macos-cross-compiler:latest aarch64-apple-darwin22-clang --target=aarch64-apple-darwin22 -framework Foundation watcher.c watcher-mac.m -o watcher-mac
-
 dir /b watcher-lin-x64 watcher-lin-arm watcher-win.exe watcher-mac
 
 pause
--- watcher/build-all.sh
+++ watcher/build-all.sh
@@ -4,6 +4,4 @@ rm watcher-lin-x64 watcher-lin-arm watcher-win.exe watcher-mac
 
 docker buildx build --output type=local,dest=./ .
 
-docker run -v .:/workspace --rm -it ghcr.io/shepherdjerred/macos-cross-compiler:latest aarch64-apple-darwin22-clang --target=aarch64-apple-darwin22 -framework Foundation watcher.c watcher-mac.m -o watcher-mac
-
 ls watcher-lin-x64 watcher-lin-arm watcher-win.exe watcher-mac
